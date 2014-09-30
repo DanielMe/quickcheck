0fe1f1e330a8c2d5b7f2699d83b2d8545f13bcd0
Daniel Mescheder <github@danielmescheder.de>
2014-09-30 03:14:21 +0200

    Added callback that collects tix info to quickCheckWithTrace function. Does not do anything useful with it at this stage, just collect and distinguish coverage from failed test runs and successful test runs

--- Test/QuickCheck/TestWithHints.hs
+++ Test/QuickCheck/TestWithHints.hs
@@ -13,6 +13,9 @@ import Test.QuickCheck.Test
 import Test.QuickCheck.Result
 import Test.QuickCheck.State
 import Trace.Hpc.Reflect
+import Trace.Hpc.Tix
+import Data.IORef
+import Control.Applicative
 
 data Trace = Trace String
 
@@ -22,16 +25,41 @@ quickCheckWithHints p = quickCheckWithTrace stdArgs p >> return ()
 
 quickCheckWithTrace :: Testable prop => Args -> prop -> IO (Result, Trace)
 quickCheckWithTrace args p = do
+  clearTix
+  emptyTix <- examineTix
+  successTix <- newIORef emptyTix
+  failureTix <- newIORef emptyTix
   let args' = args { keepGoing = True }
       hooks = Hooks { preTestHook = preTest
                     , postTestHook = postTest }
       preTest :: State -> IO ()
-      preTest st = return ()
+      preTest st = clearTix
       postTest :: State -> P.Result -> IO ()
-      postTest st res = do
-        tix <- examineTix
-        --putStrLn (show tix)
-        return ()
+      postTest st res = case P.ok res of
+                          Just True -> do
+                                       updateTixRef successTix
+                                       print "Success"                    
+                          Just False -> do
+                                        updateTixRef failureTix
+                                        print "Failure"
+                          
   res <- quickCheckWithHooks args' hooks p
+  readIORef successTix >>= print
+  readIORef failureTix >>= print
   return (res, Trace "foo")
         
+-- Tix [TickModule "name" hash length-of-list [ticks]]
+
+updateTixRef :: IORef Tix -> IO ()
+updateTixRef tixref = do
+  currentTix <- readIORef tixref
+  newTix <- examineTix
+  writeIORef tixref (addTix currentTix newTix)
+
+
+addTix :: Tix -> Tix -> Tix
+addTix (Tix modules) (Tix modules') = Tix $ zipWith addTix' modules modules'
+    where
+      addTix' (TixModule name hash len ticks) (TixModule name' _ len' ticks')
+          | name /= name' || len /= len' = error "encountered incompatible Tix definitions!"
+          | otherwise = TixModule name hash len ( zipWith (+) ticks ticks' )
